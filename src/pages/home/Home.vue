f58187ddb4635de4868fc1333e427d583ca85f92
杨益林 <1187148000@qq.com>
2018-08-31 21:44:53 +0800

    ‘init’

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,11 +1,15 @@
 <template>
 	<div>
-		 hello world
+		<home-header></home-header>
 	</div>
 </template>
 <script type="text/javascript">
+	import HomeHeader from './components/Header'
 	export default({
-		name:'Home'
+		name:'Home',
+		components:{
+			HomeHeader
+		}
 	})
 </script>
 <style scoped>
