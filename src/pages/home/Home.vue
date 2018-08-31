45ea52b8e164a766956409a1718f87a858a66cd0
杨益林 <1187148000@qq.com>
2018-08-31 18:08:25 +0800

    vue

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,18 @@
+<template>
+	<div>
+		<div class="home">
+		 	home
+		</div>
+		<router-link to="/list" class="home">列表</router-link>
+	</div>
+</template>
+<script type="text/javascript">
+	export default({
+		name:'Home'
+	})
+</script>
+<style scoped>
+	.home{
+		font-style: 50px;
+	}
+</style>
