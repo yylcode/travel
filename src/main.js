8fc0d91d6c37b41b4a6b945040d9ab1a11888b88
杨益林 <1187148000@qq.com>
2018-09-01 11:58:45 +0800

    'swiper'

--- src/main.js
+++ src/main.js
@@ -3,14 +3,16 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastClick from  'fastClick'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+
 import 'styles/reset.css'
-// 1像素边框问题的解决
 import 'styles/border.css' 
 import 'styles/iconfont.css' 
-
+import 'swiper/dist/css/swiper.css'
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
+Vue.use(VueAwesomeSwiper)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,57 @@
+<template>
+	<div class="wrapper">
+		<swiper :options="swiperOption">
+	    <!-- slides -->
+		    <swiper-slide v-for="item of swiperList"
+		    				:key="item.id" >
+		    	<img class="swiper-img" 
+				    	:src="item.imgUrl"
+				    	:alt="item.title">
+		    </swiper-slide>
+		  
+		   
+		    <div class="swiper-pagination"  slot="pagination"></div>
+	  </swiper>
+	</div>
+	
+</template>
+
+<script>
+	export default{
+		name:'HomeSwiper',
+		data(){
+			return {
+				swiperOption:{
+					pagination:'.swiper-pagination',
+					loop:true,
+					autoplay:true,
+					speed:2000
+
+				},
+				swiperList:[{
+					id:'0001',
+					title:'轮播图_1',
+					imgUrl:'http://img1.qunarzz.com/piao/fusion/1807/66/e5a5cec881702f02.jpg_750x200_67bb5691.jpg'
+				},{
+					id:'0002',
+					title:'轮播图_2',
+					imgUrl:'http://img1.qunarzz.com/piao/fusion/1808/8a/1957a0fb58ad0402.jpg_750x200_e72b8c0f.jpg'
+				}]
+			}
+		}
+	}
+</script>
+
+<style lang="stylus" scoped>
+.wrapper>>>.swiper-pagination-bullet-active
+		background:#fff !important
+.wrapper
+	overflow:hidden
+	width:100%
+	height:0
+	padding-bottom:26.67%
+	background:#ccc
+	// 高度等于窗口宽度的31.25%
+	.swiper-img
+		width:100%
+</style>
