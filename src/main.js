b9420a3212cfdecfb1548061b27bb74e5923eab5
杨益林 <1187148000@qq.com>
2018-09-01 10:42:56 +0800

    add header

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastClick from  'fastClick'
-import './assets/styles/reset.css'
+import 'styles/reset.css'
 // 1像素边框问题的解决
-import './assets/styles/border.css' 
+import 'styles/border.css' 
+import 'styles/iconfont.css' 
+
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -1,8 +1,18 @@
 <template>
 	<div class="header">
-		 <div class="header-left">返回</div>
-		 <div class="header-input">输入城市/景点/游玩主题</div>
-		 <div class="header-right">城市</div>
+		<div class="header-left">
+			<div class="iconfont back-icon">&#xe624;</div>
+		</div>
+		<div class="header-input">
+			<span class="iconfont">
+				&#xe632;
+			</span>
+			输入城市/景点/游玩主题
+		</div>
+		<div class="header-right">
+			城市
+			<span class="iconfont arrow-icon">&#xe64a;</span>
+		</div>
 	</div>
 </template>
 <script type="text/javascript">
@@ -11,26 +21,35 @@
 	})
 </script>
 <style lang="stylus" scoped>
-	 .header
-	 	display:flex
-	 	line-height:.86rem
-	 	background:#00bcd4
-	 	color:#fff
-	 	.header-left
-	 		width:.64rem
-	 		float:left
-	 	.header-input
-	 		flex:1
-	 		height:.64rem
-	 		background:#fff
-	 		border-radius:.1rem
-	 		margin-top:.12rem
-	 		line-height:.64rem
-	 		margin-left:.2rem
-	 		color:#ccc
+@import '~styles/varibles.styl'
+.header
+	display:flex
+	line-height:.86rem
+	background:$bgColor
+	color:#fff
+	.header-left
+		width:.64rem
+		float:left
+		.back-icon
+			text-align:center
+			font-size:.4rem	 
+	.header-input
+		flex:1
+		height:.64rem
+		background:#fff
+		border-radius:.1rem
+		margin-top:.12rem
+		line-height:.64rem
+		margin-left:.2rem
+		padding-left:.2rem
+		color:#ccc
 
-	 	.header-right
-	 		width:1.24rem
-	 		float:right
-	 		text-align:center
+	.header-right
+		width:1.24rem
+		float:right
+		text-align:center
+		.arrow-icon
+			font-size:.24rem
+			margin-left:-.04rem
+			
 </style>
