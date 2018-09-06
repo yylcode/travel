33d35f816212117b1ec339e6c13439c355dc8548
杨益林 <1187148000@qq.com>
2018-09-06 10:54:29 +0800

    city-search-list

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,18 +1,72 @@
 <template>
-	<div class="search">
-	 	<input class="search-input" type="text" placeholder="输入城市名或拼音"/>
+
+	<div>
+		<div class="search">
+		 	<input v-model="keyword" class="search-input" type="text" placeholder="输入城市名或拼音"/>
+		</div>
+		<div class="search-content"
+			 v-show="keyword"	
+			 ref="search">
+			<ul>
+				<li class="search-item border-bottom" 
+				v-for="item of list"
+				:key="item.id">{{item.name}}</li>
+				<li class="search-item border-bottom" v-show="hasNoData">没有找到匹配数据</li>
+			</ul>
+		</div>
 	</div>
 </template>
 <script>
+	import Bscroll from 'better-scroll'
 	export default{
-		name:'CitySearch'
+		name:'CitySearch',
+		props:{
+			cities:Object
+		},
+		data(){
+			return{
+				keyword:'',
+				list:[],
+				timer:null
+			}
+		},
+		computed:{
+			hasNoData(){
+				return !this.list.length
+			}
+		},
+		watch:{
+			keyword(){
+				if(this.timer){
+					clearTimeout(this.timere)
+				}
+				if(!this.keyword){
+					this.list=[]
+					return 
+				}
+				this.timer=setTimeout(()=>{
+					const result=[]
+					for(let i in this.cities){
+						this.cities[i].forEach((value)=>{
+							if(value.spell.indexOf(this.keyword)>-1||value.name.indexOf(this.keyword)>-1){
+								result.push(value)
+							}
+						})
+					}
+					this.list=result
+				},100)
+			}
+		},
+		mounted(){
+			this.scroll=new Bscroll(this.$refs.search)
+		}
 	}
 </script>
 
 
 <style lang="stylus" scoped>
  @import '~styles/varibles.styl'
- .search
+.search
  	padding:0 .1rem
  	background: $bgColor
  	height:.72rem
@@ -26,5 +80,18 @@
  		border-radius: .06rem
  		color:#666
  		font-size:.24rem
- 		
+.search-content
+	z-index:1
+	overflow:hidden
+	position:absolute
+	top:1.58rem
+	left:0
+	right:0
+	bottom:0
+	background: #eee
+	.search-item
+		line-height:.62rem
+		padding-left:.2rem
+		background: #fff
+		color:#666
 </style>
