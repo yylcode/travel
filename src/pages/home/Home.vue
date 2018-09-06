3d318e3e0b93d791779891d8d2e6b00f9aa2a4ea
杨益林 <1187148000@qq.com>
2018-09-06 14:54:44 +0800

    keepalive

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -14,6 +14,7 @@
 	import HomeRecommend from './components/Recommend'
 	import HomeWeekend from './components/Weekend'
 	import axios from 'axios'
+	import {mapState} from 'vuex'
 	export default{
 		name:'Home',
 		components:{
@@ -29,24 +30,33 @@
 				swiperList:[],
 				iconList:[],
 				recommendList:[],
-				weekendList:[]
+				weekendList:[],
+				lastCity:''
 			}
 		},
 		mounted()
 		{
+			this.lastCity=this.city
 			this.getHomeInfo()
+		},
+		activated(){
+			if(this.lastCity!==this.city){
+				this.lastCity=this.city
+					this.getHomeInfo()
+			}
+		},
+		computed:{
+			...mapState(['city'])
 		},
 		methods:{
 			getHomeInfo(){
-				axios.get('/api/index.json')
+				axios.get('/api/index.json?city='+this.city)
 				.then(this.getHomeInfoSucc)
 			},
-			getHomeInfoSucc(res){
-				 
+			getHomeInfoSucc(res){	 
 				res=res.data
 				if(res.ret&&res.data){
 					const data=res.data
-					
 				 	this.swiperList=data.swiperList
 				 	this.iconList=data.iconList
 				 	this.recommendList=data.recommendList
